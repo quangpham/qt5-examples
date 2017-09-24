bcf742b3280647077860a1d49422e8694ccdfadd
hunght <hunght.thaibinh@gmail.com>
2017-09-24 17:02:20 +0700

    Add Project4

---
+++ Project4/ManageDataFromUrl.cpp
@@ -0,0 +1,122 @@
+#include "ManageDataFromUrl.h"
+#include <unistd.h>
+#include <QDebug>
+#include <QtWidgets>
+#include <QFile>
+#include <QTextStream>
+
+ManageDataFromUrl::ManageDataFromUrl(QWebEngineView *view, const QUrl &url)
+{
+    m_url = url;
+    m_view = view;
+
+    connect(m_view, SIGNAL(loadFinished(bool)), this, SLOT(loadNextURl()));
+    connect(this, SIGNAL(getHTML(QString)), this, SLOT(handleHTML(QString)));
+
+    this->SendRequestToServer(m_url);
+}
+
+void ManageDataFromUrl::SendRequestToServer(const QUrl &url)
+{
+    QString data;
+    QEventLoop eventLoop;
+    QNetworkAccessManager mgr;
+    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+    QNetworkRequest req(url);
+    req.setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
+    QNetworkReply *reply = mgr.get(req);
+    eventLoop.exec(); // blocks stack until "finished()" has been called
+
+    if (reply->error() == QNetworkReply::NoError) {
+        //success
+        data = reply->readAll();
+        delete reply;
+    }
+    else {
+        //failure
+        qDebug() << "Failure" <<reply->errorString();
+        delete reply;
+    }
+
+    this->setDataReceivedFromServer(data);
+}
+
+void ManageDataFromUrl::setDataReceivedFromServer(QString data)
+{
+//    qDebug() << "Data reviced from URL:" << data;
+    m_data = data;
+    handleDataReviceFromURL(m_data);
+}
+
+void ManageDataFromUrl::handleDataReviceFromURL(QString m_data)
+{
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(m_data.toUtf8());
+    if (!jsonResponse.isObject()) {
+            qDebug() << "Document is not an object";
+        }
+    QJsonObject jsonObject = jsonResponse.object();
+    QJsonArray jsonArray = jsonObject["pages"].toArray();
+    foreach (const QJsonValue & value, jsonArray) {
+        QJsonObject obj = value.toObject();
+        m_listUrl.append(obj["url"].toString());
+
+        QJsonArray jsonArray_action = obj["action"].toArray();
+        foreach (const QJsonValue &action_element, jsonArray_action) {
+            QString jsonType = action_element.toObject()["type"].toString();
+            m_listType.append(jsonType);
+             QString jsonNameFile = action_element.toObject()["js_cmd"].toString();
+             m_listCmd.append(jsonNameFile);
+        }
+
+
+    }
+
+    for (int i = 0; i < m_listType.length(); i++) {
+        qDebug() << m_listType.at(i);
+    }
+}
+
+void ManageDataFromUrl::loadTheFirstUrl()
+{
+    m_view->load(m_listUrl.at(0));
+}
+
+void ManageDataFromUrl::loadNextURl()
+{
+    m_view->page()->toHtml([this](const QString& result) mutable {emit getHTML(result);});
+    if (curentUrlIndex < m_listUrl.length()) {
+         m_view->load(m_listUrl.at(curentUrlIndex));
+         curentUrlIndex++;
+    }
+}
+
+void ManageDataFromUrl::handleHTML(QString sHTML)
+{
+
+}
+
+void ManageDataFromUrl::writeContentHTMLtoFile(QString fileName, QString data)
+{
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text) ) {
+        qDebug() << "Couldn't open file for writing";
+        return;
+    }
+
+    QTextStream out(&file);
+    out << data;
+    file.flush();
+    file.close();
+}
+
+void ManageDataFromUrl::readContentHTMLfromFile(QString fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        qDebug() << "Coudln't open file for reading";
+        return;
+    }
+    QTextStream in(&file);
+    QString myText = in.readAll();
+    qDebug() << myText;
+}
---
+++ Project4/main.cpp
@@ -0,0 +1,41 @@
+/***********************************************************
+Author: hunght
+Date: 19-09-2017
+Email: hunght.thaibinh@gmail.com
+http://telekmedia.com/qt/amazon-1.json
+Load json
+Open urls
+Save content to id.html
+***********************************************************/
+
+#include <QApplication>
+#include <QtWidgets>
+#include "mainwindow.h"
+#include <qtwebenginewidgetsglobal.h>
+//#include <QtNetwork/QNetworkProxy>
+
+#define SERVER_URL "http://telekmedia.com/qt/facebook-2.json"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication a(argc, argv);
+    QUrl url;
+    if (argc > 1)
+        url = QUrl::fromUserInput(argv[1]);
+    else
+        url = QUrl(SERVER_URL);
+    MainWindow *browser = new MainWindow(url);
+    browser->show();
+
+//    QNetworkProxy proxy;
+
+//    proxy = QNetworkProxy::HttpProxy;
+//    proxy.setHostName("46.101.24.27");
+//    proxy.setPort(3128);
+//    proxy.setUser("");
+//    proxy.setPassword("");
+//    QNetworkProxy::setApplicationProxy(proxy);
+
+    return a.exec();
+}
